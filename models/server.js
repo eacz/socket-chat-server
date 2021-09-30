fc9be7cccaf676944b234504cbbe9d12b1cc2c96
Esteban Canteros <estebanacanteros@gmail.com>
2021-09-29 22:01:33 -0300

    setup

---
+++ models/server.js
@@ -0,0 +1,42 @@
+const express  = require('express');
+const http     = require('http');
+const socketio = require('socket.io');
+const path     = require('path');
+
+const Sockets  = require('./sockets');
+
+class Server {
+
+    constructor() {
+
+        this.app  = express();
+        this.port = process.env.PORT;
+
+        this.server = http.createServer( this.app );
+        
+        this.io = socketio( this.server, { /* configurations */ } );
+    }
+
+    middlewares() {
+        this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
+    }
+
+    configurateSockets() {
+        new Sockets( this.io );
+    }
+
+    execute() {
+
+        this.middlewares();
+
+        this.configurateSockets();
+
+        this.server.listen( this.port, () => {
+            console.log('Server running on:', this.port );
+        });
+    }
+
+}
+
+
+module.exports = Server;
