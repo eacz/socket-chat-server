bcb4751f4d5f86e8c64a60b65bd12ea52093ab25
Esteban Canteros <estebanacanteros@gmail.com>
2021-10-02 15:52:41 -0300

    custom middleware validateFields

--- routers/auth.js
+++ routers/auth.js
@@ -4,16 +4,27 @@ const { Router } = require('express')
 const { check } = require('express-validator')
 const router = Router()
 const { createUser, login, renewToken } = require('../controllers/auth')
+const validateFields = require('../middlewares/validateFields')
 
-router.post('/new', createUser)
+router.post('/new', [
+  check('email', 'The email is required').isEmail(),
+  check('password', 'The password must be at least 8 characters').isLength({ min: 8 }),
+  check('name', 'The name is required').notEmpty(),
+  check('username', 'The username is required').notEmpty(),
+  validateFields
 
-router.post('/', 
-[
-  check('email', 'The email is required').isEmail(),  
-  check('password', 'The password must be at least 8 characters').isLength({min: 8}),  
-]
-,login)
+], createUser)
+
+router.post(
+  '/',
+  [
+    check('email', 'The email is required').isEmail(),
+    check('password', 'The password must be at least 8 characters').isLength({ min: 8 }),
+    validateFields,
+  ],
+  login
+)
 
 router.get('/renew', renewToken)
 
-module.exports = router;
+module.exports = router
